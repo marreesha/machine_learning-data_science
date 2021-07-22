cb1727945465906f0fe4475e962aa841babe7cfe
marreesha <48161214+marreesha@users.noreply.github.com>
2021-07-22 13:25:36 +0300

    Create build_ziggurat.R

---
+++ stepik-R/build_ziggurat.R
@@ -0,0 +1,19 @@
+build_ziggurat <- function(n) {
+  size = 2*n-1
+  m = matrix(1, nrow = size, ncol = size)
+  for(i in 1:(n-1)) {
+    m[(1 + i):(size - i), (1 + i):(size - i)] <- i+1
+  }
+}
+
+
+#print(build_ziggurat(4))
+
+#[,1] [,2] [,3] [,4] [,5] [,6] [,7]
+#[1,]    1    1    1    1    1    1    1
+#[2,]    1    2    2    2    2    2    1
+#[3,]    1    2    3    3    3    2    1
+#[4,]    1    2    3    4    3    2    1
+#[5,]    1    2    3    3    3    2    1
+#[6,]    1    2    2    2    2    2    1
+#[7,]    1    1    1    1    1    1    1
